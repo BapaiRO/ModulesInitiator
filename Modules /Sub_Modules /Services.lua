cecf1810f3236d99e684fcfab3beec81647f3ce5
vasilythetoiletdestroyer <bapaipovestitorul@gmail.com>
2026-06-30 16:34:08 +0300

    Create Services.lua

---
+++ Modules /Sub_Modules /Services.lua
@@ -0,0 +1,19 @@
+local module = {
+	
+	Players = game:GetService("Players"),
+	
+	ReplicatedStorage = game:GetService("ReplicatedStorage"),
+	
+	StarterGui = game:GetService("StarterGui"),
+	
+	TWS = game:GetService("TweenService"),
+	
+	SoundService = game:GetService("SoundService"),
+	
+	UIS = game:GetService("UserInputService"),
+	
+	TeleportService = game:GetService("TeleportService")
+	
+}
+
+return module
